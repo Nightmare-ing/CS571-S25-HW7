04d904417930dbb395b4d0842423342f5363af2d
Qingsong Hu <kingsanhu@gmail.com>
2026-06-27 19:59:00 +0800

    Finish Step 1: Display Items

--- src/components/BadgerMart.jsx
+++ src/components/BadgerMart.jsx
@@ -1,13 +1,29 @@
 import { Text, View } from "react-native";
 import BadgerSaleItem from "./BadgerSaleItem";
 
-import CS571 from '@cs571/mobile-client'
+import CS571 from "@cs571/mobile-client";
+import { useEffect, useState } from "react";
 
 export default function BadgerMart(props) {
-    return <View>
-        <Text style={{fontSize: 28}}>Welcome to Badger Mart!</Text>
-        <BadgerSaleItem/>
-        <BadgerSaleItem/>
-        <BadgerSaleItem/>
-    </View>
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch("https://cs571.org/rest/s25/hw7/items", {
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId(),
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setItems(data);
+            });
+    }, []);
+
+    return (
+        <View>
+            <Text style={{ fontSize: 28 }}>Welcome to Badger Mart!</Text>
+            {items ?
+                <BadgerSaleItem {...items[0]} />
+            :   <Text>Loading...</Text>}
+        </View>
+    );
 }
--- src/components/BadgerSaleItem.jsx
+++ src/components/BadgerSaleItem.jsx
@@ -1,8 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, Dimensions } from "react-native";
 
 export default function BadgerSaleItem(props) {
-
-    return <View>
-        <Text>I am an item!</Text>
-    </View>
+    return (
+        <View style={{ alignItems: "center" }}>
+            <Image
+                style={{
+                    width: "60%",
+                    aspectRatio: 1,
+                }}
+                source={{ uri: props.imgSrc }}
+                resizeMode="cover"
+            />
+            <Text
+                style={{
+                    fontSize: "36",
+                    margin: "8",
+                }}
+            >
+                {props.name}
+            </Text>
+            <Text style={{ fontSize: "24", margin: "8" }}>
+                ${props.price?.toFixed(2)} each
+            </Text>
+            <Text style={{ fontSize: "16", margin: "8" }}>
+                You can order up to {props.upperLimit} units!
+            </Text>
+        </View>
+    );
 }
